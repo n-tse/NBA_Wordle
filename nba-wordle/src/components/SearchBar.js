a3155b03b71d97ec190a83fd36573bdfe6f53bcf
Nick Tse <nicholashtse@gmail.com>
2022-12-11 16:55:23 -0800

    add basic search bar component

---
+++ nba-wordle/src/components/SearchBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function SearchBar({ placeholder, data }) {
+  return (
+    <div className="search">
+      <div className="searchInputs">
+        <input type="text"/>
+        <div className="searchIcon"></div>
+      </div>
+      <div className="dataResult"></div>
+    </div>
+  );
+}
+
+export default SearchBar;
