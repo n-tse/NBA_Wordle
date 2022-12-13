147eac78b48f35b0701aa45f9caea2f1211bd892
Nick Tse <nicholashtse@gmail.com>
2022-12-12 20:50:15 -0800

    update search dropdown so that only matches are displayed

--- nba-wordle/src/components/SearchBar.js
+++ nba-wordle/src/components/SearchBar.js
@@ -1,28 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './SearchBar.css';
 import SearchIcon from '@mui/icons-material/Search';
 
 function SearchBar({ placeholder, data }) {
+  
+  const [filteredData, setFilteredData] = useState([]);
+  const handleFilter = (event) => {
+    const searchName = event.target.value
+    const newFilter = data.filter((value) => {
+      return (value.first_name.toLowerCase() + " " + value.last_name.toLowerCase()).includes(searchName.toLowerCase());
+    });
+    if (searchName === "") {
+      setFilteredData([]);
+    } else {
+      setFilteredData(newFilter);
+    }
+  }
   return (
     <div className="search">
       <div className="searchInputs">
         <input 
           type="text"
           placeholder={placeholder}
+          onChange={handleFilter}
         />
         <div className="searchIcon">
           <SearchIcon />
         </div>
       </div>
-      <div className="dataResult">
-        {data.map((value, key) => {
-          return (
-            <div className="dataItem"> 
-              <p> {value.first_name + " " + value.last_name} </p> 
-            </div>
-          );
-        })}
-      </div>
+      {filteredData.length !== 0 && (
+        <div className="dataResult">
+          {filteredData.map((value, key) => {
+            return (
+              <div className="dataItem"> 
+                <p> {value.first_name + " " + value.last_name} </p> 
+              </div>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 }
